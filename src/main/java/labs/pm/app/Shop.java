cd568d6febdea43d89113656692b82d648aafc53
Vlad Marii <mariyvladyslav@gmail.com>
2024-07-06 05:44:33 +0300

    arrays for reviews added

--- src/main/java/labs/pm/app/Shop.java
+++ src/main/java/labs/pm/app/Shop.java
@@ -16,6 +16,7 @@ import labs.pm.data.Rating;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Locale;
 
 /**
  * {@code Shop} class represents an application that manages Products
@@ -25,30 +26,39 @@ import java.time.LocalDate;
  */
 public class Shop {
     public static void main(String[] args) {
-        ProductManager pm = new ProductManager();
+        ProductManager pm = new ProductManager(Locale.US);
         Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.THREE_STAR);
-        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
-        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR,
-                LocalDate.now().plusDays(2));
-        Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR, LocalDate.now());
-        Product p5 = p3.applyRating(Rating.THREE_STAR);
-        Product p6 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
-        Product p7 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR,
-                LocalDate.now().plusDays(2));
-        Product p8 = p4.applyRating(Rating.FIVE_STAR);
-        Product p9 = p1.applyRating(Rating.ONE_STAR);
+        pm.printProductReport();
+        p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Nice hot cup of tea");
+        p1 = pm.reviewProduct(p1, Rating.TWO_STAR, "Rather weak tea");
+        p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Fine tea");
+        p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Good tea");
+        p1 = pm.reviewProduct(p1, Rating.FIVE_STAR, "Perfect tea");
+        p1 = pm.reviewProduct(p1, Rating.THREE_STAR, "Just add some lemon");
+        pm.printProductReport();
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
-        System.out.println(p4);
-        System.out.println(p5);
-        System.out.println(p8);
-        System.out.println(p9);
-
-        if (p3 instanceof Food) {
-            System.out.println(((Food) p3).getBestBefore());
-        }
-        System.out.println(p1.getBestBefore());
+//        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
+//        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR,
+//                LocalDate.now().plusDays(2));
+//        Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR, LocalDate.now());
+//        Product p5 = p3.applyRating(Rating.THREE_STAR);
+//        Product p6 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
+//        Product p7 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR,
+//                LocalDate.now().plusDays(2));
+//        Product p8 = p4.applyRating(Rating.FIVE_STAR);
+//        Product p9 = p1.applyRating(Rating.ONE_STAR);
+//
+//        System.out.println(p1);
+//        System.out.println(p2);
+//        System.out.println(p3);
+//        System.out.println(p4);
+//        System.out.println(p5);
+//        System.out.println(p8);
+//        System.out.println(p9);
+//
+//        if (p3 instanceof Food) {
+//            System.out.println(((Food) p3).getBestBefore());
+//        }
+//        System.out.println(p1.getBestBefore());
     }
 }
